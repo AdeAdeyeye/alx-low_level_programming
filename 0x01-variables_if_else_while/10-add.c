f2ed43014ade0b27513879b7ee5398f679ec18e4
AdeAdeyeye <adeadeyeyeadeleye@gmail.com>
2022-10-19 03:58:42 -0700

    loop 10

--- 0x01-variables_if_else_while/10-add.c
+++ 0x01-variables_if_else_while/10-add.c
@@ -4,7 +4,7 @@
  * @num1: The first integer to be added.
  * @num2: The second integer to be added.
  *
- * Return: The result of the addition.
+ * Return: Returns the sum of num1 and num2
  */
 int add(int num1, int num2)
 {
