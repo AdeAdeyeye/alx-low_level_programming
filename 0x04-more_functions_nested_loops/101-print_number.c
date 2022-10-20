fd37d9153a674f408dda6e9f5bc4d1b7d0e209da
AdeAdeyeye <adeadeyeyeadeleye@gmail.com>
2022-10-20 12:58:41 -0700

    Numbers have life

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,10 +1,10 @@
-nclude "main.h"
+#include "main.h"
+
 /**
  * print_number - prints an integer
- * @n:.input integer parameter
+ *  @n:.input integer parameter
  *
  */
-
 void print_number(int n)
 {
 	unsigned int i = n;
