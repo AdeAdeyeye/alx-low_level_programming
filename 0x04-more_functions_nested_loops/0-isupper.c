90ea2a71e7026946566b24bc2a22fcbad2cfd5fd
AdeAdeyeye <adeadeyeyeadeleye@gmail.com>
2022-10-20 05:38:01 -0700

    isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,15 +1,17 @@
 #include "main.h"
 
 /**
- * _isupper - Check if a letter is uppercase
+ * _isupper - uppercase letters
+ * @c: character to check
  *
- * Return: 1 for upper letter or 0 for any else
+ * Return: 0 or 1
  */
-int _isupper(int c);
+int _isupper(int c)
+
 {
-	if (x >= 65 && x <= 90)
-	{
+	if (c >= 'A' && c <= 'Z')
 		return (1);
-	}
-	return (0);
+	else
+		return (0);
+
 }
