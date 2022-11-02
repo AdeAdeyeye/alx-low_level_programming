4cde7696389cc4342de6579a76013e678af7a1a1
AdeAdeyeye <adeadeyeyeadeleye@gmail.com>
2022-11-02 01:44:58 -0700

    puts_recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,23 @@
+/*
+ * File: 0-puts_recursion.c
+ */
+
+#include "main.h"
+
+/**
+ * _puts_recursion - print a string
+ * @s: string
+ * Return:void
+ */
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+	{
+		_putchar("\n");
+	}
+}
