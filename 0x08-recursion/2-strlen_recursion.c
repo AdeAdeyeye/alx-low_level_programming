e3769063e44ffdf4994831e9c6e9d4724679e4b7
AdeAdeyeye <adeadeyeyeadeleye@gmail.com>
2022-11-02 04:10:23 -0700

    2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,17 +1,15 @@
 #include "main.h"
 /**
- * _strlen_recursion - Returns the length of a string.
- * @s: The string to be measured.
- *
- * Return: The length of the string.
+ * _print_rev_recursion - print a string in reverse
+ * @s: string
+ * Return 0;
  */
-int _strlen_recursion(char *s)
-{
-	int len = 0;
 
+void _print_rev_recursion(char *s)
+{
 	if (*s)
 	{
-		len++;
-		len += _strlen_recursion(s + 1);
+		_putchar(*s);
+		_print_rev_recursion(s - 1);
 	}
 }
