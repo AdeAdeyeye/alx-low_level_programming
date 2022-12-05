6fa7b9b6f22ffa423f1750eaaea7debf1296016e
LeeHarvard <ade.adeyeyeadeleye@gmail.com>
2022-12-05 12:01:22 -0800

    task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -26,6 +26,28 @@ void check97(int argc)
  *
  * Return: void
  */
+void check98(ssize_t check, char *file, int fd_from, int fd_to)
+{
+	if (check == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file);
+		if (fd_from != -1)
+			close(fd_from);
+		if (fd_to != -1)
+			close(fd_to);
+		exit(98);
+	}
+}
+
+/**
+ * check99 - checks that file_to was created and/or can be written to
+ * @check: checks if true of false
+ * @file: file_to name
+ * @fd_from: file descriptor of file_from, or -1
+ * @fd_to: file descriptor of file_to, or -1
+ *
+ * Return: void
+ */
 void check99(ssize_t check, char *file, int fd_from, int fd_to)
 {
 	if (check == -1)
